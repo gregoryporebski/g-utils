a92b6318cb41bf01f8bb579c4ad8f967a9596374
Gregory <gregory.porebski@gmail.com>
2023-12-07 23:48:21 +0100

    fix: Export missing `clone` types

--- src/clone/index.ts
+++ src/clone/index.ts
@@ -2,3 +2,4 @@ export { default as clone } from "./clone";
 export { default as cloneDeep } from "./cloneDeep";
 export { default as cloneDeepWith } from "./cloneDeepWith";
 export { default as cloneWith } from "./cloneWith";
+export * from "./types";
