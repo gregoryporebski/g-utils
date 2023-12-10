93e82f8f128ba0d3a05dc54fd3264fae5e2c4b64
Gregory <gregory.porebski@gmail.com>
2023-12-11 00:42:19 +0100

    test: Extended `merge` tests

--- src/merge/merge.test.ts
+++ src/merge/merge.test.ts
@@ -234,234 +234,310 @@ describe("merge", () => {
       expect(merge(obj1, obj2)).toEqual({ a: false });
     });
 
-    test("symbol properties", () => {
+    test("array properties", () => {
+      const obj1 = { a: [1, 2] };
+      const obj2 = { a: [3, 4] };
+      expect(merge(obj1, obj2)).toEqual({ a: [1, 2, 3, 4] });
+    });
+
+    test("object properties", () => {
+      const obj1 = { a: { b: 1 } };
+      const obj2 = { a: { c: 2 } };
+      expect(merge(obj1, obj2)).toEqual({ a: { b: 1, c: 2 } });
+    });
+
+    test("function properties", () => {
+      const obj1 = { a: () => {} };
+      const obj2 = { a: () => {} };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
+
+    test("Date properties", () => {
+      const obj1 = { a: new Date(2021, 1, 1) };
+      const obj2 = { a: new Date(2022, 1, 1) };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
+
+    test("RegExp properties", () => {
+      const obj1 = { a: /lorem/ };
+      const obj2 = { a: /ipsum/ };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
+
+    test("Symbol properties", () => {
       const obj1 = { a: Symbol("lorem") };
       const obj2 = { a: Symbol("ipsum") };
       expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
     });
 
-    test("symbol keys", () => {
+    test("Symbol keys", () => {
       const key1 = Symbol("lorem");
       const key2 = Symbol("ipsum");
       const obj1 = { [key1]: "hello" };
       const obj2 = { [key2]: "world" };
       expect(merge(obj1, obj2)).toEqual({ [key1]: "hello", [key2]: "world" });
     });
-  });
 
-  test("overwrite properties", () => {
-    const obj1 = { a: 1 };
-    const obj2 = { a: 2 };
-    expect(merge(obj1, obj2)).toEqual({ a: 2 });
-  });
-
-  test("nested objects", () => {
-    const obj1 = { a: { b: 1 } };
-    const obj2 = { a: { c: 2 } };
-    expect(merge(obj1, obj2)).toEqual({ a: { b: 1, c: 2 } });
-  });
-
-  test("overwrite nested properties", () => {
-    const obj1 = { a: { b: 1 } };
-    const obj2 = { a: { b: 2 } };
-    expect(merge(obj1, obj2)).toEqual({ a: { b: 2 } });
-  });
-
-  test("arrays", () => {
-    const obj1 = { a: [1, 2] };
-    const obj2 = { a: [3, 4] };
-    expect(merge(obj1, obj2)).toEqual({ a: [1, 2, 3, 4] });
-    expect(merge(obj2, obj1)).toEqual({ a: [3, 4, 1, 2] });
-  });
-
-  test("null values", () => {
-    const obj1 = { a: null };
-    const obj2 = { b: 2 };
-    expect(merge(obj1, obj2)).toEqual({ a: null, b: 2 });
-  });
-
-  test("undefined values", () => {
-    const obj1 = { a: undefined };
-    const obj2 = { b: 2 };
-    expect(merge(obj1, obj2)).toEqual({ a: undefined, b: 2 });
-  });
-
-  test("empty objects", () => {
-    const obj1 = {};
-    const obj2 = { b: 2 };
-    expect(merge(obj1, obj2)).toEqual({ b: 2 });
-    expect(merge(obj2, obj1)).toEqual({ b: 2 });
-  });
-
-  test("merging with a null object", () => {
-    const obj1 = { a: 1 };
-    const obj2 = null;
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with an undefined object", () => {
-    const obj1 = { a: 1 };
-    const obj2 = undefined;
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a non-object", () => {
-    const obj1 = { a: 1 };
-    const obj2 = 2;
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a function", () => {
-    const obj1 = { a: 1 };
-    const obj2 = () => {};
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a date object", () => {
-    const obj1 = { a: 1 };
-    const obj2 = new Date();
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with an array", () => {
-    const obj1 = { a: 1 };
-    const obj2 = [1, 2, 3];
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a string", () => {
-    const obj1 = { a: 1 };
-    const obj2 = "hello";
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a boolean", () => {
-    const obj1 = { a: 1 };
-    const obj2 = true;
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a symbol", () => {
-    const obj1 = { a: 1 };
-    const obj2 = Symbol("symbol");
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging with a BigInt", () => {
-    const obj1 = { a: 1 };
-    const obj2 = BigInt(123);
-    expect(merge(obj1, obj2)).toEqual({ a: 1 });
-    expect(merge(obj2, obj1)).toEqual({ a: 1 });
-  });
-
-  test("merging objects with Symbol properties", () => {
-    const obj1 = { a: Symbol("symbol1") };
-    const obj2 = { a: Symbol("symbol2") };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
-
-  test("merging objects with Date properties", () => {
-    const obj1 = { a: new Date(2021, 1, 1) };
-    const obj2 = { a: new Date(2022, 1, 1) };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
-
-  test("merging objects with function properties", () => {
-    const obj1 = { a: () => {} };
-    const obj2 = { a: () => {} };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
-
-  test("merging objects with RegExp properties", () => {
-    const obj1 = { a: /abc/ };
-    const obj2 = { a: /def/ };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
-
-  test("merging objects with Set properties", () => {
-    const obj1 = { a: new Set([1, 2, 3]) };
-    const obj2 = { a: new Set([4, 5, 6]) };
-    expect(merge(obj1, obj2)).toEqual({ a: new Set([1, 2, 3, 4, 5, 6]) });
-    expect(merge(obj2, obj1)).toEqual({ a: new Set([4, 5, 6, 1, 2, 3]) });
-  });
-
-  test("merging objects with Map properties", () => {
-    const obj1 = { a: new Map().set("key1", "value1") };
-    const obj2 = { a: new Map().set("key2", "value2") };
-    expect(merge(obj1, obj2)).toEqual({
-      a: new Map([
-        ["key1", "value1"],
-        ["key2", "value2"],
-      ]),
+    test("BigInt properties", () => {
+      const obj1 = { a: BigInt(123) };
+      const obj2 = { a: BigInt(456) };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
     });
-    expect(merge(obj2, obj1)).toEqual({
-      a: new Map([
-        ["key2", "value2"],
-        ["key1", "value1"],
-      ]),
+
+    test("Map properties", () => {
+      const obj1 = { a: new Map().set("key1", "value1") };
+      const obj2 = { a: new Map().set("key2", "value2") };
+      expect(merge(obj1, obj2)).toEqual({
+        a: new Map([
+          ["key1", "value1"],
+          ["key2", "value2"],
+        ]),
+      });
     });
-  });
 
-  test("merging objects with Promise properties", () => {
-    const obj1 = { a: Promise.resolve("value1") };
-    const obj2 = { a: Promise.resolve("value2") };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
+    test("WeakMap properties", () => {
+      const obj1 = { a: new WeakMap() };
+      const obj2 = { a: new WeakMap() };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
 
-  test("merging objects with BigInt properties", () => {
-    const obj1 = { a: BigInt(123) };
-    const obj2 = { a: BigInt(456) };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
+    test("Set properties", () => {
+      const obj1 = { a: new Set().add(1) };
+      const obj2 = { a: new Set().add(2) };
+      expect(merge(obj1, obj2)).toEqual({ a: new Set([1, 2]) });
+    });
 
-  test("merging objects with Buffer properties", () => {
-    const obj1 = { a: Buffer.from("value1") };
-    const obj2 = { a: Buffer.from("value2") };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
+    test("WeakSet properties", () => {
+      const obj1 = { a: new WeakSet() };
+      const obj2 = { a: new WeakSet() };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
 
-  test("merging objects with ArrayBuffer properties", () => {
-    const obj1 = { a: new ArrayBuffer(8) };
-    const obj2 = { a: new ArrayBuffer(8) };
-    expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
-    expect(merge(obj2, obj1)).toEqual({ a: obj1.a });
-  });
+    test("Promise properties", () => {
+      const obj1 = { a: new Promise(() => "lorem") };
+      const obj2 = { a: new Promise(() => "ipsum") };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
 
-  test("merging objects with NaN properties", () => {
-    const obj1 = { a: NaN };
-    const obj2 = { a: NaN };
-    expect(merge(obj1, obj2)).toEqual({ a: NaN });
-    expect(merge(obj2, obj1)).toEqual({ a: NaN });
-  });
+    test("Error properties", () => {
+      const obj1 = { a: new Error("lorem") };
+      const obj2 = { a: new Error("ipsum") };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
 
-  test("merging objects with Infinity properties", () => {
-    const obj1 = { a: Infinity };
-    const obj2 = { a: Infinity };
-    expect(merge(obj1, obj2)).toEqual({ a: Infinity });
-    expect(merge(obj2, obj1)).toEqual({ a: Infinity });
-  });
+    test("URL properties", () => {
+      const obj1 = { a: new URL("http://lorem.com") };
+      const obj2 = { a: new URL("http://ipsum.com") };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
 
-  test("merging objects with -Infinity properties", () => {
-    const obj1 = { a: -Infinity };
-    const obj2 = { a: -Infinity };
-    expect(merge(obj1, obj2)).toEqual({ a: -Infinity });
-    expect(merge(obj2, obj1)).toEqual({ a: -Infinity });
+    test("URLSearchParams properties", () => {
+      const obj1 = { a: new URLSearchParams("lorem=1") };
+      const obj2 = { a: new URLSearchParams("ipsum=2") };
+      expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+    });
+
+    test("Infinity properties", () => {
+      const obj1 = { a: Infinity };
+      const obj2 = { a: -Infinity };
+      expect(merge(obj1, obj2)).toEqual({ a: -Infinity });
+    });
+
+    test("NaN properties", () => {
+      const obj1 = { a: NaN };
+      const obj2 = { a: -NaN };
+      expect(merge(obj1, obj2)).toEqual({ a: -NaN });
+    });
+
+    test("Buffer properties", () => {
+      const obj1 = { a: Buffer.from("lorem") };
+      const obj2 = { a: Buffer.from("ipsum") };
+      const merged = merge(obj1, obj2);
+      expect(merged).toEqual({ a: obj2.a });
+      expect(merged.a).toBe(obj2.a);
+    });
+
+    test("ArrayBuffer properties", () => {
+      const obj1 = { a: new ArrayBuffer(4) };
+      const obj2 = { a: new ArrayBuffer(8) };
+      const merged = merge(obj1, obj2);
+      expect(merged).toEqual({ a: obj2.a });
+      expect(merged.a).toBe(obj2.a);
+    });
+
+    test("SharedArrayBuffer properties", () => {
+      const obj1 = { a: new SharedArrayBuffer(4) };
+      const obj2 = { a: new SharedArrayBuffer(8) };
+      const merged = merge(obj1, obj2);
+      expect(merged).toEqual({ a: obj2.a });
+      expect(merged.a).toBe(obj2.a);
+    });
+
+    test("DataView properties", () => {
+      const obj1 = { a: new DataView(new ArrayBuffer(4)) };
+      const obj2 = { a: new DataView(new ArrayBuffer(8)) };
+      const merged = merge(obj1, obj2);
+      expect(merged).toEqual({ a: obj2.a });
+      expect(merged.a).toBe(obj2.a);
+    });
+
+    describe("typed arrays", () => {
+      test("Int8Array", () => {
+        const obj1 = { a: new Int8Array(4) };
+        const obj2 = { a: new Int8Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Uint8Array", () => {
+        const obj1 = { a: new Uint8Array(4) };
+        const obj2 = { a: new Uint8Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Uint8ClampedArray", () => {
+        const obj1 = { a: new Uint8ClampedArray(4) };
+        const obj2 = { a: new Uint8ClampedArray(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Int16Array", () => {
+        const obj1 = { a: new Int16Array(4) };
+        const obj2 = { a: new Int16Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Uint16Array", () => {
+        const obj1 = { a: new Uint16Array(4) };
+        const obj2 = { a: new Uint16Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Int32Array", () => {
+        const obj1 = { a: new Int32Array(4) };
+        const obj2 = { a: new Int32Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Uint32Array", () => {
+        const obj1 = { a: new Uint32Array(4) };
+        const obj2 = { a: new Uint32Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Float32Array", () => {
+        const obj1 = { a: new Float32Array(4) };
+        const obj2 = { a: new Float32Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("Float64Array", () => {
+        const obj1 = { a: new Float64Array(4) };
+        const obj2 = { a: new Float64Array(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+    });
+
+    describe("mixed properties", () => {
+      test("undefined and null", () => {
+        const obj1 = { a: undefined };
+        const obj2 = { a: null };
+        expect(merge(obj1, obj2)).toEqual({ a: null });
+      });
+
+      test("string and number", () => {
+        const obj1 = { a: "hello" };
+        const obj2 = { a: 123 };
+        expect(merge(obj1, obj2)).toEqual({ a: 123 });
+      });
+
+      test("boolean and array", () => {
+        const obj1 = { a: true };
+        const obj2 = { a: [1, 2] };
+        expect(merge(obj1, obj2)).toEqual({ a: [1, 2] });
+      });
+
+      test("object and function", () => {
+        const obj1 = { a: { b: 1 } };
+        const obj2 = { a: () => {} };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Date and RegExp", () => {
+        const obj1 = { a: new Date(2021, 1, 1) };
+        const obj2 = { a: /lorem/ };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Symbol and BigInt", () => {
+        const obj1 = { a: Symbol("lorem") };
+        const obj2 = { a: BigInt(123) };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Map and WeakMap", () => {
+        const obj1 = { a: new Map() };
+        const obj2 = { a: new WeakMap() };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Set and WeakSet", () => {
+        const obj1 = { a: new Set() };
+        const obj2 = { a: new WeakSet() };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Promise and Error", () => {
+        const obj1 = { a: undefined };
+        const obj2 = { a: /lorem/ };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("URL and URLSearchParams", () => {
+        const obj1 = { a: new URL("http://lorem.com") };
+        const obj2 = { a: new URLSearchParams("ipsum=2") };
+        expect(merge(obj1, obj2)).toEqual({ a: obj2.a });
+      });
+
+      test("Infinity and NaN", () => {
+        const obj1 = { a: Infinity };
+        const obj2 = { a: NaN };
+        expect(merge(obj1, obj2)).toEqual({ a: NaN });
+      });
+
+      test("Buffer and ArrayBuffer", () => {
+        const obj1 = { a: Buffer.from("lorem") };
+        const obj2 = { a: new ArrayBuffer(8) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+
+      test("SharedArrayBuffer and DataView", () => {
+        const obj1 = { a: new SharedArrayBuffer(8) };
+        const obj2 = { a: new DataView(new ArrayBuffer(8)) };
+        const merged = merge(obj1, obj2);
+        expect(merged).toEqual({ a: obj2.a });
+        expect(merged.a).toBe(obj2.a);
+      });
+    });
   });
 
   describe("crazy objects with", () => {
@@ -486,6 +562,57 @@ describe("merge", () => {
       const obj1 = Object.create(proto);
       const obj2 = Object.create(proto);
       expect(merge(obj1, obj2)).toEqual({});
+    });
+
+    test("circular references", () => {
+      const obj1: any = { a: 1 };
+      const obj2: any = { b: 2 };
+      obj1.c = obj2;
+      obj2.d = obj1;
+      expect(merge(obj1, obj2)).toEqual({ a: 1, b: 2, c: obj2, d: obj1 });
+    });
+
+    test("getter/setter properties", () => {
+      const obj1 = {
+        get prop() {
+          return 1;
+        },
+        set prop(value) {
+          (this as any)._prop = value;
+        },
+      };
+      const obj2 = { prop: 2 };
+      expect(merge(obj1, obj2)).toEqual({ prop: 2 });
+    });
+
+    test("non-enumerable properties", () => {
+      const obj1 = { a: 1 };
+      const obj2 = { b: 2 };
+      Object.defineProperty(obj1, "a", { enumerable: false });
+      Object.defineProperty(obj2, "b", { enumerable: false });
+      const merged = merge(obj1, obj2) as any;
+      expect(merged.a).toEqual(obj1.a);
+      expect(merged.b).toEqual(obj2.b);
+    });
+
+    test("non-configurable properties", () => {
+      const obj1 = { a: 1 };
+      const obj2 = { b: 2 };
+      Object.defineProperty(obj1, "a", { configurable: false });
+      Object.defineProperty(obj2, "b", { configurable: false });
+      const merged = merge(obj1, obj2) as any;
+      expect(merged.a).toEqual(obj1.a);
+      expect(merged.b).toEqual(obj2.b);
+    });
+
+    test("non-writable properties", () => {
+      const obj1 = { a: 1 };
+      const obj2 = { b: 2 };
+      Object.defineProperty(obj1, "a", { writable: false });
+      Object.defineProperty(obj2, "b", { writable: false });
+      const merged = merge(obj1, obj2) as any;
+      expect(merged.a).toEqual(obj1.a);
+      expect(merged.b).toEqual(obj2.b);
     });
   });
 });
